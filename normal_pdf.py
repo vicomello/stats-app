852634a77b4f882a3a80e2c964d6631ea0c8308a
hauselin <hauselin@gmail.com>
2021-02-06 13:24:48 -0600

    prettify

--- normal_pdf.py
+++ normal_pdf.py
@@ -8,12 +8,31 @@ import pandas as pd
 import altair as alt
 from scipy.stats import norm
 
+#%% config
+
+st.set_page_config(
+    page_title="your_title",
+    layout="wide",
+    initial_sidebar_state="auto",
+)
+
+#%% title
+
+st.title("Normal distribution")
+st.write(
+    "Use the sliders to change the mean and standard deviation of the normal distribution. The figure is interactive, so you can hover over it, zoom in/out, or drag it around."
+)
+
+#%% side select box
+
+add_selectbox = st.sidebar.selectbox("Learn more about...", ("a", "b", "c"))
+
 #%% slider
 
 mu_values = [-100.0, 100.0]
 mu_values.append(float(np.median(mu_values)))
 
-sigma_values = [1.0, 100.0, 15.0]
+sigma_values = [1.0, 50.0, 15.0]
 
 mu = st.slider(f"Mean of distribution", *mu_values)
 st.write("Mean: ", mu)
@@ -29,6 +48,8 @@ df = pd.DataFrame({"x": x, "y": y})
 
 #%% plot and display
 
+st.write("#")  # add break
+
 # https://stackoverflow.com/questions/52223358/rename-tooltip-in-altair
 fig = (
     alt.Chart(df)
@@ -36,7 +57,7 @@ fig = (
     .encode(
         x=alt.X(
             "x",
-            scale=alt.Scale(domain=(mu_values[0] * 4, mu_values[1] * 4)),
+            scale=alt.Scale(domain=(mu_values[0], mu_values[1])),
             axis=alt.Axis(title="Mean"),
         ),
         y=alt.Y("y", axis=alt.Axis(title="Normal probability density")),
