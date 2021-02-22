d5a2a654be793b7b49ce33917da77095722f4e47
Victoria Oldemburgo de Mello <43184812+vicomello@users.noreply.github.com>
2021-02-21 19:02:59 -0500

    including regression line file

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
+#%%
+
 import streamlit as st
 import numpy as np
 import pandas as pd
 
+#%%
 sd = st.slider('sd')
 data = np.random.normal(0, sd, 1000)
 
 st.subheader('Distribution as a function of sd')
 hist_values = np.histogram(data, bins=50)[0]
-st.bar_chart(hist_values)
+st.bar_chart(hist_values, width=10, height=300)
+
+# %%
+ stats.norm.pdf(x, mu, sigma)
+ pd.DataFrame()
+ax.plot()
+fig.show() ##shows figure on console
+
+# draw linear function in graph
+# people can decide intercept and slope
+# B0 and B1
+# go to altair 
